22a4c11467f598d80b30bf140d0c857e451a1501
MinPan9585 <panmin677095@gmail.com>
2025-07-15 20:58:14 +0800

    add air wall in scene, add shadow

--- Assets/Scripts/Shadows.cs
+++ Assets/Scripts/Shadows.cs
@@ -7,6 +7,15 @@ public class Shadows : MonoBehaviour
     public Transform head;
     public Transform leftHand;
     public Transform rightHand;
+
+    public int index;
+
+    float shadowMaxZ = 0.1f;
+    float shadowMinZ = 0.05f;
+    float shadowMaxY = 7f;
+    float shadowMinY = 4.3f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +25,26 @@ public class Shadows : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.GetChild(0).position = new Vector3(head.position.x, head.position.y, 2.8f);
+        
+        if (index == 0)
+        {
+            transform.position = new Vector3(28.6f, head.position.y, head.position.z);
+
+        }
+        if (index == 1)
+        {
+            transform.position = new Vector3(28.6f, leftHand.position.y, leftHand.position.z);
+        }
+        if (index == 2)
+        {
+            transform.position = new Vector3(28.6f, rightHand.position.y, rightHand.position.z);
+        }
+
+        if (transform.position.y > shadowMaxY || transform.position.y < shadowMinY)
+        {
+            transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, shadowMinY, shadowMaxY), transform.position.z);
+        }
+        //if
+
     }
 }
